fdb920f6a74e86e77dbe8c96c4e93ef4c93b7300
shyluthomas <shylupthomas@gmail.com>
2023-11-23 21:09:44 +0000

    the firebse api with node completed
    Signed-off-by: shyluthomas <shylupthomas@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -25,9 +25,111 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({origin: true}));
+const db = admin.firestore();
 
 app.get('/', (req,res) => {
 return res.status(200).send('server running');
 })
+
+// Post
+app.post("/api/create", (req, res) => {
+    (async () => {
+      try {
+        await db.collection("userdetails").doc(`/${Date.now()}/`).create({
+          id: Date.now(),
+          name: req.body.name,
+          mobile: req.body.mobile,
+          address: req.body.address,
+        });
+  
+        return res.status(200).send({ status: "Success", msg: "Data Saved" });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "Failed", msg: error });
+      }
+    })();
+  });
+  
+  // read specific user detail
+  // get
+  app.get("/api/userDetail/:id", (req, res) => {
+    (async () => {
+      try {
+        const reqDoc = db.collection("userdetails").doc(req.params.id);
+        let userDetail = await reqDoc.get();
+        let response = userDetail.data();
+  
+        return res.status(200).send({ status: "Success", data: response });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "Failed", msg: error });
+      }
+    })();
+  });
+  
+  // read all user details
+  // get
+  app.get("/api/userDetails", (req, res) => {
+    (async () => {
+      try {
+        let query = db.collection("userdetails");
+        let response = [];
+  
+        await query.get().then((data) => {
+          let docs = data.docs; // query results
+  
+          docs.map((doc) => {
+            const selectedData = {
+              name: doc.data().name,
+              mobile: doc.data().mobile,
+              address: doc.data().address,
+            };
+  
+            response.push(selectedData);
+          });
+          return response;
+        });
+  
+        return res.status(200).send({ status: "Success", data: response });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "Failed", msg: error });
+      }
+    })();
+  });
+  
+  // update
+  // put
+  app.put("/api/update/:id", (req, res) => {
+    (async () => {
+      try {
+        const reqDoc = db.collection("userdetails").doc(req.params.id);
+        await reqDoc.update({
+          name: req.body.name,
+          mobile: req.body.mobile,
+          address: req.body.address,
+        });
+        return res.status(200).send({ status: "Success", msg: "Data Updated" });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "Failed", msg: error });
+      }
+    })();
+  });
+  
+  // delete
+  // delete
+  app.delete("/api/delete/:id", (req, res) => {
+    (async () => {
+      try {
+        const reqDoc = db.collection("userdetails").doc(req.params.id);
+        await reqDoc.delete();
+        return res.status(200).send({ status: "Success", msg: "Data Removed" });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "Failed", msg: error });
+      }
+    })();
+  });
 
 exports.app = onRequest(app);
